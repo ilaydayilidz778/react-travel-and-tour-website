d04c3e5367537d77f6e12fb15e8fdc4f9251818b
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-03-01 00:20:07 +0300

    Started main.scss for Main.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import video from '../../assets/images/video.mp4';
 import { GrLocation } from "react-icons/gr";
 import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
 import './home.scss'
 
 const Home = () => {
@@ -46,6 +51,19 @@ const Home = () => {
                     </div>
                     <div className="searchOptions flex">
                         <HiFilter className='icon' />
+                        <span>MORE FILTERS</span>
+                    </div>
+                </div>
+
+                <div className="homeFooterIcons flex">
+                    <div className="rightIcons">
+                        <FiFacebook className='icon' />
+                        <AiOutlineInstagram className='icon' />
+                        <FaTripadvisor className='icon' />
+                    </div>
+                    <div className="leftIcons">
+                        <BsListTask className='icon' />
+                        <TbApps className='icon' />
                     </div>
                 </div>
             </div>
